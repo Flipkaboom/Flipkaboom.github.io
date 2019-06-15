693de984a45538cd0a170414e0ad5c506d746fbf
Flipkaboom <32464794+Flipkaboom@users.noreply.github.com>
2019-06-15 16:21:40 +0200

    Add files via upload

--- TheGame/main.js
+++ TheGame/main.js
@@ -7,11 +7,9 @@ function preload() {
   }
   sounds = [];
   sounds.length = 0;
-  for(var i = 0; i < 3; i++){
+  for(var i = 0; i < 4; i++){
     var b = new Audio("Sound/" + i + ".mp3");
     sounds.push(b);
-
-
   }
 }
 
@@ -19,18 +17,15 @@ function setup(){
   canvas = createCanvas(1920,1080);
   angleMode(DEGREES);
   frameRate(60);
-
   scene = 0;
   drawList = [];
   functionList = [];
-
 
   setInterval(geluidScene1,1000);
 
 //array
 
   scenes = []
-
     scenes[0] = {
       background:images[0],
       coinBagFunction(){
@@ -48,17 +43,62 @@ function setup(){
       },
       startButton:new Item(751,424,images[3],function(){if(frameCount % 60 == 0){this.invisible = false; geluidScene1();}else if(frameCount % 60 == 30){this.invisible = true;}},0,0,false)
     }
-
+//beginscene------------------------------------------------------------------
     scenes[0].coinBag = new MouseHover(1515,249,images[2],scenes[0].coinBagFunction,333,505,true);
     scenes[0].coin = new Draggable(1688,619,images[1],0,0,true,function(){if(this.x + (0.5*this.width) < scenes[0].coinBag.x || this.x + this.width > scenes[0].coinBag.x + scenes[0].coinBag.width || this.y < scenes[0].coinBag.y || this.y + this.height > scenes[0].coinBag.y + scenes[0].coinBag.height){this.invisible = false;}});
-    scenes[0].coinslot = new Target(1300,953,0,0,scenes[0].coin);
+    scenes[0].coinslot = new Target(1225,923,0,function(){nextScene();},scenes[0].coin,120,140);
+//----------------------------------------------------------------------------
+    scenes[1] = {
+      background:images[1]
+    }
+    scenes[1].oilBarrel = new Draggable(0,0,images[1],0,0,0,false);
+    scenes[1].refinery = new Target(0,0,images[1],0,scenes[1].oilBarrel,0,0,false);
+    scenes[1].infoRefinery = new MouseHover(0,0,images[1],function(){if(this.hover == true){this.invisible=false;} else{invisible = true;}},0,0,true);
+//2e scene---------------------------------------------------------------------
+scenes[2] = {
+  background:images[1]
+}
+    // scenes[2].oven = new Target(x,y,i,f,r,w,h,inv);
+    // scenes[2].polyetheleen = new Draggable(x,y,i,w,h,inv,f);
+    // scenes[2].infoPolyetheleen = new MouseHover(x,y,i,f,w,h,inv);
+//3e scene----------------------------------------------------------------------
+
+
+    //MouseHover (x,y,i,f,w,h,inv)
+    //Clickable (x,y,i,f,w,h,inv)
+    //Draggable (x,y,i,w,h,inv,f)
+    //Target (x,y,i,f,r,w,h,inv)
+    //Item (x,y,i,f,w,h,inv)
+
+    addToLists();
+
+    //einde setup
 }
 
 
+function nextScene(){
+  scene++;
+  drawList.length = 0;
+  functionList.length = 0;
+  addToLists();
+  // sounds[3].play();
+  jemoeder
+}
+
+function addToLists(){
+  for (var property in scenes[scene]) {
+    try{
+      scenes[0][property].addLists();
+    }
+    catch{
+
+    }
+  }
+}
 
   function geluidScene1(){
     if (scene == 0) {
-      sounds[1].play();
+      sounds[1].play();  //hier moet pling spelen
 
     }
   }
@@ -73,7 +113,7 @@ function setup(){
   }
 
 function draw(){
-  background(images[0]);
+  background(scenes[scene].background);
   for(var i = 0; i < functionList.length; i++){
       functionList[i].loop();
   }
@@ -81,16 +121,10 @@ function draw(){
       render(drawList[i]);
   }
   // fill("black");
-  // rect(1515,249,333,505);
+  // rect(1225,923,120,140);
+  sounds[3].play();
 }
 
-// function placeholderBewegen(){
-//   if(mouseX > this.x && mouseX < this.x + this.width && mouseY > this.y && mouseY < this.y + this.height) {
-//   this.x = mouseX;
-//   this.y = mouseY;
-//
-//   }
-// }
 function render(obj){
   if(obj.invisible != true){
     image(obj.image,obj.x,obj.y);
@@ -104,13 +138,18 @@ class MouseHover{
     this.image = i;
     this.function = f;
     this.invisible = inv;
+    this.hover = false;
     if(this.image != 0){
-      drawList.push(this);
       this.width = this.image.width;
       this.height = this.image.height;
     }else{
       this.width = w;
       this.height = h;
+    }
+  }
+  addLists(){
+    if(this.image != 0){
+      drawList.push(this);
     }
     functionList.push(this);
   }
@@ -132,12 +171,16 @@ class Clickable{
     this.function = f;
     this.invisible = inv;
     if(this.image != 0){
-      drawList.push(this);
       this.width = this.image.width;
       this.height = this.image.height;
     }else{
       this.width = w;
       this.height = h;
+    }
+  }
+  addLists(){
+    if(this.image != 0){
+      drawList.push(this);
     }
     functionList.push(this);
   }
@@ -153,12 +196,16 @@ class Draggable{
     this.invisible = inv;
     this.function = f;
     if(this.image != 0){
-      drawList.push(this);
       this.width = this.image.width;
       this.height = this.image.height;
     }else{
       this.width = w;
       this.height = h;
+    }
+  }
+  addLists(){
+    if(this.image != 0){
+      drawList.push(this);
     }
     functionList.push(this);
   }
@@ -188,12 +235,16 @@ class Target{
     this.function = f;
     this.invisible = inv;
     if(this.image != 0){
-      drawList.push(this);
       this.width = this.image.width;
       this.height = this.image.height;
     }else{
       this.width = w;
       this.height = h;
+    }
+  }
+  addLists(){
+    if(this.image != 0){
+      drawList.push(this);
     }
     functionList.push(this);
   }
@@ -201,7 +252,7 @@ class Target{
     this.checkRequirement();
   }
   checkRequirement(){
-    if(this.requirement.x > this.x && this.requirement.x + this.requirement.width < this.x && this.requirement.y > this.y && this.requirement.y + this.requirement.height < this.y ){
+    if(this.requirement.x + (0.5 * this.requirement.width) > this.x && this.requirement.x + (0.5*this.requirement.width) < this.x + this.width && this.requirement.y + (0.5 * this.requirement.height) > this.y && this.requirement.y + (0.5*this.requirement.height) < this.y + this.height){
       this.function();
     }
   }
@@ -215,12 +266,16 @@ class Item{
     this.loop = f;
     this.invisible = inv;
     if(this.image != 0){
-      drawList.push(this);
       this.width = this.image.width;
       this.height = this.image.height;
     }else{
       this.width = w;
       this.height = h;
+    }
+  }
+  addLists(){
+    if(this.image != 0){
+      drawList.push(this);
     }
     if(this.loop != 0){
       functionList.push(this);
